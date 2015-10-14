c04bf45c77da2905ab7221e82b0bb8841cece2f7
Michael McClure <SalamanderMike@gmail.com>
2015-10-14 17:58:46 +0800

    Fixed Minor Angular Router Bug

--- routes/routes.js
+++ routes/routes.js
@@ -9,13 +9,9 @@ router.get('/site', function (req,res) {
 	res.render('site');
 });
 
-// exports.routes = function(req, res){
-//   res.render('site');
-// };
-
-// router.get('/*', function (req, res) { 
-//     return res.sendFile(path.join(config.root, 'site.ejs')); 
-// });
+router.get('/*', function (req,res) {
+	res.redirect('/site');
+});
 
 
 
