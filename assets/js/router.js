5eeda9f306a18f07930d5dce881480aa708645d3
Michael McClure <SalamanderMike@gmail.com>
2015-10-15 16:22:20 +0800

    Fixed Angular Router Wiring Bug

--- assets/js/router.js
+++ assets/js/router.js
@@ -1,21 +1,28 @@
-var Router = angular.module('Router', ['ngRoute']).
-    config(['$routeProvider', '$locationProvider', function($routeProvider,$locationProvider) {
-        $routeProvider.
-            when('/', {
-                templateUrl: 'site',
-                controller: 'AppController',
-                controllerAs: 'app'
-            }).
-            otherwise({
-                templateUrl: 'site'
+angular.module('Router', ['ngRoute'])
+    .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl:    'site',
+                controller:     'AppController',
+                controllerAs:   'app'
+            })
+            .when('/test', {
+                templateUrl:    'test',
+                controller:     'AppController',
+                controllerAs:   'app'
+            })
+            .otherwise({
+                templateUrl:    'site',
+                controller:     'AppController',
+                controllerAs:   'app'
             });
         $locationProvider.html5Mode({
             enabled: true,
             requireBase: false
         });
 
-    }]).
-    config([
+    }])
+    .config([
     "$httpProvider", 
     function($httpProvider) {
         return $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,23 @@
 var express = require('express'),
 	router = express.Router();
 
-router.get('/', function (req,res) {
-	res.redirect('/site');
+router.get('/test', function (req,res) {
+	res.render('test');
 });
 
-router.get('/site', function (req,res) {
+router.get('*', function (req,res) {
 	res.render('site');
 });
 
-
+// The following will download the file onto your computer instead of rendering it
+//
+// var path = require('path');
+// var root_path = {
+//     root: path.join(__dirname, '../views/')			// CHANGES THE ROOT PATH TO A STEP OUTSIDE OF /routes, THEN INTO /views
+// };
+// router.get('/download', function (req, res) {		// WHEN NAVIGATING TO /download FILE WILL DOWNLOAD
+//     res.sendFile('site.ejs', root_path);
+// });
 
 module.exports = router;
 
