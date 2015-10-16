70b200948e2c89067c5c2594a7c6b9865cda6535
Michael McClure <SalamanderMike@gmail.com>
2015-10-16 10:13:41 +0800

    Toying With Multiple Controllers Not Working Yet

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -1,20 +1,19 @@
-angular.module('Controller', [])
+angular.module('Controllers', [])
 	.controller('AppController', function($scope) {
 		var app = this;
-		$scope.format = 'M/d/yyyy h:mm:ss a';								// DATE AND TIME FORMAT
 
 
 // SECTION: TODO
-		app.todos = [																				// Array of items for our Todo List
+		app.todos = [													// Array of items for our Todo List
 			{text:'Item #1', done:true},
 			{text:'Item #2', done:false}];
 
-		app.addTodo = function() {													// Function to add to our list
+		app.addTodo = function() {										// Function to add to our list
 			app.todos.push({text:app.todoText, done:false});
 			app.todoText = '';
 		};
 
-		app.remaining = function() {												// Function to find number of unchecked items
+		app.remaining = function() {									// Function to find number of unchecked items
 			var count = 0;
 			angular.forEach(app.todos, function(todo) {
 				count += todo.done ? 0 : 1;
@@ -22,7 +21,7 @@ angular.module('Controller', [])
 			return count;
 		};
 
-		app.clear = function() {														// Function to remove done items
+		app.clear = function() {										// Function to remove done items
 			var allTodos = app.todos;
 			app.todos = [];
 			angular.forEach(allTodos, function(todo) {
@@ -30,4 +29,7 @@ angular.module('Controller', [])
 			});
 		};
 
+	})
+	.controller('DateCtrl', function($scope) {
+		$scope.format = 'M/d/yyyy h:mm:ss a';							// DATE AND TIME FORMAT
 	});
